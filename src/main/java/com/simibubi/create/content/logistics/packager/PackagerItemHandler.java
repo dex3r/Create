c5728c223e7940e6eeb3318e6c45ad6250406a51
TropheusJ <jverrellijr@icloud.com>
2025-08-25 21:39:08 -0400

    fix extracting from packagers deleting packages

--- src/main/java/com/simibubi/create/content/logistics/packager/PackagerItemHandler.java
+++ src/main/java/com/simibubi/create/content/logistics/packager/PackagerItemHandler.java
@@ -1,5 +1,7 @@
 package com.simibubi.create.content.logistics.packager;
 
+import java.util.Optional;
+
 import com.simibubi.create.content.logistics.box.PackageItem;
 
 import net.minecraft.world.item.ItemStack;
@@ -8,21 +10,33 @@ import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.StoragePreconditions;
 import net.fabricmc.fabric.api.transfer.v1.storage.base.SingleSlotStorage;
 import net.fabricmc.fabric.api.transfer.v1.transaction.TransactionContext;
+import net.fabricmc.fabric.api.transfer.v1.transaction.base.SnapshotParticipant;
 
 import io.github.fabricators_of_create.porting_lib.transfer.callbacks.TransactionCallback;
 
-public class PackagerItemHandler implements SingleSlotStorage<ItemVariant> {
+public class PackagerItemHandler extends SnapshotParticipant<Optional<ItemStack>> implements SingleSlotStorage<ItemVariant> {
 
 	private final PackagerBlockEntity blockEntity;
+
+	private Optional<ItemStack> newHeldBox = Optional.empty();
 
 	public PackagerItemHandler(PackagerBlockEntity blockEntity) {
 		this.blockEntity = blockEntity;
 	}
 
+	private ItemStack getHeldBox() {
+		return this.newHeldBox.orElse(this.blockEntity.heldBox);
+	}
+
+	private void setHeldBox(ItemStack stack) {
+		this.newHeldBox = Optional.of(stack);
+	}
+
 	@Override
 	public long insert(ItemVariant resource, long maxAmount, TransactionContext transaction) {
 		StoragePreconditions.notBlankNotNegative(resource, maxAmount);
-		if (!blockEntity.heldBox.isEmpty() || !blockEntity.queuedExitingPackages.isEmpty())
+		ItemStack box = this.getHeldBox();
+		if (!box.isEmpty() || !blockEntity.queuedExitingPackages.isEmpty())
 			return 0;
 		if (!PackageItem.isPackage(resource))
 			return 0;
@@ -40,12 +54,16 @@ public class PackagerItemHandler implements SingleSlotStorage<ItemVariant> {
 		StoragePreconditions.notBlankNotNegative(resource, maxAmount);
 		if (blockEntity.animationTicks != 0)
 			return 0;
-		ItemStack box = blockEntity.heldBox;
+		ItemStack box = this.getHeldBox();
 		if (!resource.matches(box))
 			return 0;
 
-		blockEntity.heldBox = ItemStack.EMPTY;
-		TransactionCallback.onSuccess(transaction, blockEntity::notifyUpdate);
+		int toExtract = (int) Math.min(maxAmount, box.getCount());
+		int newSize = box.getCount() - toExtract;
+
+		this.updateSnapshots(transaction);
+		this.setHeldBox(box.copyWithCount(newSize));
+
 		return box.getCount();
 	}
 
@@ -67,5 +85,25 @@ public class PackagerItemHandler implements SingleSlotStorage<ItemVariant> {
 	@Override
 	public long getCapacity() {
 		return 1;
+	}
+
+	@Override
+	protected void readSnapshot(Optional<ItemStack> snapshot) {
+		this.newHeldBox = snapshot;
+	}
+
+	@Override
+	protected Optional<ItemStack> createSnapshot() {
+		return this.newHeldBox;
+	}
+
+	@Override
+	protected void onFinalCommit() {
+		this.newHeldBox.ifPresent(stack -> {
+			this.blockEntity.heldBox = stack;
+			this.blockEntity.notifyUpdate();
+		});
+
+		this.newHeldBox = Optional.empty();
 	}
 }
